17cf188b183cf58fca792885fb47138d8d64d9c7
Max Ng <maxwindiff@gmail.com>
2025-09-29 19:41:45 -0700

    word-count

--- rust/word-count/src/lib.rs
+++ rust/word-count/src/lib.rs
@@ -2,5 +2,14 @@ use std::collections::HashMap;
 
 /// Count occurrences of words.
 pub fn word_count(words: &str) -> HashMap<String, u32> {
-    todo!("Count of occurrences of words in {words:?}");
+    words
+        .split(|c: char| !c.is_alphanumeric() && c != '\'')
+        .filter_map(|word| {
+            let word = word.trim_matches('\'').to_lowercase();
+            (!word.is_empty()).then_some(word)
+        })
+        .fold(HashMap::new(), |mut counts, word| {
+            *counts.entry(word).or_insert(0) += 1;
+            counts
+        })
 }
