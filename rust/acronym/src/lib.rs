2318672be21bfcdcece8cd1c2d1316239a177567
Max Ng <maxwindiff@gmail.com>
2025-09-29 18:58:36 -0700

    acronym

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn abbreviate(phrase: &str) -> String {
-    todo!("Given the phrase '{phrase}', return its acronym");
+    phrase
+        .split(|c: char| c.is_whitespace() || c == '-')
+        .filter_map(|word| {
+            let first_alpha = word.find(|c: char| c.is_alphanumeric())?;
+            let first = word[first_alpha..first_alpha+1].to_ascii_uppercase().to_string();
+            if word.chars().all(|c| c.is_uppercase()) {
+                Some(first)
+            } else {
+                Some(first + &word[first_alpha + 1..].chars().filter(|c| c.is_uppercase()).collect::<String>())
+            }
+        })
+        .collect()
 }
