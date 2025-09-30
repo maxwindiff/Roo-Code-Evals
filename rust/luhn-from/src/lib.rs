8a6104af4d2b967b47420d355a6e7cc24f2ebf76
Max Ng <maxwindiff@gmail.com>
2025-09-30 00:28:18 -0700

    luhn-from

--- rust/luhn-from/src/lib.rs
+++ rust/luhn-from/src/lib.rs
@@ -1,8 +1,12 @@
-pub struct Luhn;
+use std::fmt::Display;
+
+pub struct Luhn {
+    valid: bool
+}
 
 impl Luhn {
     pub fn is_valid(&self) -> bool {
-        todo!("Determine if the current Luhn struct contains a valid credit card number.");
+        self.valid
     }
 }
 
@@ -11,8 +15,28 @@ impl Luhn {
 /// by hand for every other type presented in the test suite,
 /// but your solution will fail if a new type is presented.
 /// Perhaps there exists a better solution for this problem?
-impl From<&str> for Luhn {
-    fn from(input: &str) -> Self {
-        todo!("From the given input '{input}' create a new Luhn struct.");
+impl<T: Display> From<T> for Luhn {
+    fn from(input: T) -> Self {
+        let s = input.to_string();
+        if s.find(|c: char| !c.is_whitespace() && !c.is_numeric()).is_some() {
+            return Luhn{valid: false};
+        }
+
+        let mut d: Vec<u32> = s.chars().filter_map(|c| c.to_digit(10)).collect();
+        if d.len() <= 1 {
+            return Luhn{valid: false};
+        }
+
+        let mut total = 0;
+        let mut multiplier = 1;
+        while let Some(digit) = d.pop() {
+            let mut sum = multiplier * digit;
+            if sum > 9 {
+                sum -= 9;
+            }
+            total += sum;
+            multiplier = 3 - multiplier;
+        }
+        Luhn{valid: total % 10 == 0}
     }
 }
